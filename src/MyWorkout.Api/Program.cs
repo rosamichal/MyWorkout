454f4705bda7a124df9d2b5fe5c8e72c2e15be0e
Michał Rosa <Michal.Rosa@action.pl>
2023-02-13 16:08:21 +0100

    Added initial projects

---
+++ src/MyWorkout.Api/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "MyWorkout API",
+        Description = "An ASP.NET Core Web API for managing workout plans",
+        Contact = new OpenApiContact
+        {
+            Name = "Micha³ Rosa",
+            Url = new Uri("https://www.linkedin.com/in/rosamichal/")
+        }
+    });
+
+    var filePath = Path.Combine(AppContext.BaseDirectory, "MyWorkout.xml");
+    options.IncludeXmlComments(filePath);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
