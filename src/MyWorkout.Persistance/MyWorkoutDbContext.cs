b67b1a6da96be833cdcd29e422da1c988256e185
Michał Rosa <Michal.Rosa@action.pl>
2023-02-14 15:05:21 +0100

    Added base DbContext

---
+++ src/MyWorkout.Persistance/MyWorkoutDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWorkout.Persistance
+{
+    internal class MyWorkoutDbContext : DbContext
+    {
+        public MyWorkoutDbContext(DbContextOptions<MyWorkoutDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
