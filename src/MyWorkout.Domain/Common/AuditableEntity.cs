8e7416c70ba39987a9a12a05120d47fa30109722
Michał Rosa <Michal.Rosa@action.pl>
2023-02-14 15:12:12 +0100

    Added AuditableEntity

---
+++ src/MyWorkout.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWorkout.Domain.Common
+{
+    internal class AuditableEntity
+    {
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? Modified { get; set; }
+        public bool IsDeleted { get; set; }
+        public string DeletedBy { get; set; }
+        public DateTime? Deleted { get; set; }
+    }
+}
