61eb471e80290439a21061afa8d9553ff3d792d2
Michał Rosa <Michal.Rosa@action.pl>
2023-02-20 14:46:53 +0100

    Added ExercisesController with basic implementation

---
+++ src/MyWorkout.Api/Controllers/ExercisesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using MyWorkout.Application.Common.Wrappers;
+using MyWorkout.Application.Features.Exercises.Queries.GetAllExercises;
+
+namespace MyWorkout.Api.Controllers
+{
+    [Route("api/exercises")]
+    public class ExercisesController : BaseController
+    {
+        /// <summary>
+        /// Returns all exercises for dropdown list
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse<List<ExerciseListItemViewModel>>>> GetAll()
+        {
+            var result = await Mediator.Send(new GetAllExercisesQuery());
+
+            return Ok(result);
+        }
+    }
+}
