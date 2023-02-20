4736772f8a22031c07aba0c472ebfd7da1c1a133
Michał Rosa <Michal.Rosa@action.pl>
2023-02-20 16:00:48 +0100

    Added WorkoutPlan queries

---
+++ src/MyWorkout.Application/Features/WorkoutPlans/Queries/GetAllWorkoutPlans/WorkoutPlanListItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MyWorkout.Application.Common.Mappings;
+using MyWorkout.Domain.Entities;
+
+namespace MyWorkout.Application.Features.WorkoutPlans.Queries.GetAllWorkoutPlans
+{
+    public class WorkoutPlanListItemViewModel : IMapFrom<WorkoutPlan>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
