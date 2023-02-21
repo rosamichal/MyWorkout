385da4089f7ed3a54cc0fc6eede5dc0893cdd4df
Michał Rosa <Michal.Rosa@action.pl>
2023-02-21 13:02:51 +0100

    Added configuration to WorkoutTrainingSeries and WorkoutPlanTrainingSeries

---
+++ src/MyWorkout.Persistance/Configurations/WorkoutPlanTrainingSeriesConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using MyWorkout.Domain.Entities;
+
+namespace MyWorkout.Persistance.Configurations
+{
+    public class WorkoutPlanTrainingSeriesConfiguration : IEntityTypeConfiguration<WorkoutPlanTrainingSeries>
+    {
+        public void Configure(EntityTypeBuilder<WorkoutPlanTrainingSeries> builder)
+        {
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(200);
+            builder.Property(p => p.RestTimeBetweenExercisesInSecond).IsRequired();
+            builder.Property(p => p.RestTimeBetweenSeriesInSecond).IsRequired();
+        }
+    }
+}
